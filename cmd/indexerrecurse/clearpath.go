b2fa628ebcdf5c4f48f52df9fe3ba8beb4640f51
Juergen Enge <you@example.com>
2025-03-09 14:06:53 +0100

    replace '

--- cmd/indexerrecurse/clearpath.go
+++ cmd/indexerrecurse/clearpath.go
@@ -16,7 +16,7 @@ var directCleanRuleWhitespace = regexp.MustCompile("[\u0009\u000a-\u000d\u0020\u
 var directCleanRuleEqual = regexp.MustCompile("=(u[a-zA-Z0-9]{4})")
 
 // var directCleanRule_1_5 = regexp.MustCompile("[\u0000-\u001F\u007F\n\r\t*?:\\[\\]\"<>|(){}&'!\\;#@]")
-var directCleanRule_1_5 = regexp.MustCompile("[\u0000-\u001F\u007F\n\r\t*?:\"<>|{}!\\;#]")
+var directCleanRule_1_5 = regexp.MustCompile("[\u0000-\u001F\u007F\n\r\t*?:\"<>|{}'!\\;#]")
 
 // var directCleanRule_2_4_6 = regexp.MustCompile("^[\\-~\u0009\u000a-\u000d\u0020\u0085\u00a0\u1680\u2000-\u200f\u2028\u2029\u202f\u205f\u3000]*(.*?)[\u0009\u000a-\u000d\u0020\u0085\u00a0\u1680\u2000-\u20a0\u2028\u2029\u202f\u205f\u3000]*$")
 var directCleanRule_2_4_6 = regexp.MustCompile("^[~\u0009\u000a-\u000d\u0020\u0085\u00a0\u1680\u2000-\u200f\u2028\u2029\u202f\u205f\u3000]*(.*?)[\u0009\u000a-\u000d\u0020\u0085\u00a0\u1680\u2000-\u20a0\u2028\u2029\u202f\u205f\u3000]*$")
